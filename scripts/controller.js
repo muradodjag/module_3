243807d6fe9a26ccb53b7755552a801c64158dcd
muradodjag <muradodjag@gmail.com>
2022-12-05 21:25:07 +0400

    minor fix

--- scripts/controller.js
+++ scripts/controller.js
@@ -7,19 +7,31 @@ export default class Controller {
         this.view.bindInputAmount2(this.handleChanges)
         this.view.bindClickRate1(this.handleChanges)
         this.view.bindClickRate2(this.handleChanges)
+        this.handleChanges(1, 'RUB', 'USD');
     }
 
     handleChanges = async (amount, rate1, rate2) => {
         const converted = await this.model.convertValue(amount, rate1, rate2)
         if (this.view.from) {
             console.log(converted[0])
-            this.view.amount2.value = converted[0]
+            if (+converted[0] % 1 === 0) {
+                this.view.amount2.value = converted[0]
+            }
+            else {
+                this.view.amount2.value = converted[0].toFixed(4)
+            }
             this.view.converterText1.textContent = `1 ${rate1} = ${converted[1]} ${rate2}`
             this.view.converterText2.textContent = `1 ${rate2} = ${1 / converted[1]} ${rate1}`
         }
         else {
             console.log(converted[0])
-            this.view.amount1.value = converted[0]
+            if (+converted[0] % 1 === 0) {
+                this.view.amount1.value = converted[0]
+            }
+            else {
+                this.view.amount1.value = converted[0].toFixed(4)
+            }
+
             this.view.converterText2.textContent = `1 ${rate1} = ${converted[1]} ${rate2}`
             this.view.converterText1.textContent = `1 ${rate2} = ${1 / converted[1]} ${rate1}`
         }
